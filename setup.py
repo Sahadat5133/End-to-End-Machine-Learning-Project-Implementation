c118d567b3cf476d4bcc5a1e6e34a39fabf037e7
Sahadat5133 <sahadat137839@gmail.com>
2025-08-26 17:52:40 +0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Sahadat Hossain",
+    author_email="sahadat137839@gmail.com",
+    packages=find_packages()
+)
