755e7f359aefae3e0f2b25d709998e425424da7c
Sahadat5133 <sahadat137839@gmail.com>
2025-08-26 17:31:24 +0600

    folder structure added

--- template.py
+++ template.py
@@ -30,7 +30,19 @@ list_of_files = [
     "setup.py",
     "config/model.yaml",
     "config/schema.yaml",
-
+     "test.py"
 
  ]
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
 
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open (filepath,'w') as f:
+            pass
+            
+    else:
+        print(f"{filename} is already present{filedir}, skipping file creation")
